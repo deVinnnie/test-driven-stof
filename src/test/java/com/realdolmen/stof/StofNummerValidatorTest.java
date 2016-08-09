ae589b1bd5fff3b7f9735e6a142a5fe6b9eccadb
Vincent Ceulemans <vincent.ceulemans.120@gmail.com>
2016-08-09 10:23:07 +0200

    Split up tests in seperate methods

--- src/test/java/com/realdolmen/stof/StofNummerValidatorTest.java
+++ src/test/java/com/realdolmen/stof/StofNummerValidatorTest.java
@@ -1,19 +1,28 @@
 package com.realdolmen.stof;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class StofNummerValidatorTest {
 
+    StofNummerValidator validator;
+
+    @Before
+    public void setUp(){
+         this.validator = new StofNummerValidator();
+    }
+
     @Test
-    public void testValidatorStofNummer(){
-        StofNummerValidator validator = new StofNummerValidator();
+    public void testValidateStofNummer_valid_stof_nummer_returns_true() {
         assertEquals(true, validator.validateStofNummer("STOF/1/2/3"));
         assertEquals(true, validator.validateStofNummer("STOF/A/B/C"));
-        assertEquals(false, validator.validateStofNummer("Hello"));
-        assertEquals(false, validator.validateStofNummer(null));
+    }
 
+    @Test
+    public void testValidateStofNummer_invalid_stof_nummer_returns_false(){
+        assertEquals(false, validator.validateStofNummer("Hello"));
         assertEquals(false, validator.validateStofNummer("STOF/2/3"));
         assertEquals(false, validator.validateStofNummer("STOF/AA/B/C"));
         assertEquals(false, validator.validateStofNummer("STOF/11/2/3"));
@@ -22,4 +31,8 @@ public class StofNummerValidatorTest {
         assertEquals(false, validator.validateStofNummer("STOF/1/2/31"));
     }
 
+    @Test
+    public void testValidateStofNummer_null_value_returns_false(){
+        assertEquals(false, validator.validateStofNummer(null));
+    }
 }
