4cef3df28ef90cc6b1235b855e29b01928e08f90
Vincent Ceulemans <vincent.ceulemans.120@gmail.com>
2016-08-09 11:56:10 +0200

    Add database empty check

--- src/test/java/com/realdolmen/stof/StofRepositoryTest.java
+++ src/test/java/com/realdolmen/stof/StofRepositoryTest.java
@@ -1,12 +1,18 @@
 package com.realdolmen.stof;
 
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.math.BigInteger;
 
 import static org.junit.Assert.assertNotEquals;
 
@@ -19,10 +25,13 @@ public class StofRepositoryTest {
     @Autowired
     StofRepository stofRepository;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Test
     public void testCreateStof(){
         // 1. Check if database is empty
-
+        assertStofCount(0);
 
         // 2. Create Stof instance and persist
         Stof stof = new Stof();
@@ -35,4 +44,8 @@ public class StofRepositoryTest {
         assertNotEquals(null, stof.getId());
     }
 
+    private void assertStofCount(int count){
+        Object result = entityManager.createNativeQuery("SELECT COUNT(1) FROM Stof").getSingleResult();
+        Assert.assertEquals(new BigInteger("" + count), result);
+    }
 }
