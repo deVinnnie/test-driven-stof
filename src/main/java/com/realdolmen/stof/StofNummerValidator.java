1d85ac05029ecde6e07a1915b1ca5d054b042f83
Vincent Ceulemans <vincent.ceulemans.120@gmail.com>
2016-08-09 10:00:07 +0200

    Test and fix stofValidator

--- src/main/java/com/realdolmen/stof/StofNummerValidator.java
+++ src/main/java/com/realdolmen/stof/StofNummerValidator.java
@@ -1,8 +1,15 @@
 package com.realdolmen.stof;
 
+import java.util.regex.Pattern;
+
 public class StofNummerValidator {
 
+    private static final Pattern PATTERN = Pattern.compile("STOF/([a-zA-Z0-9])/([a-zA-Z0-9])/([a-zA-Z0-9])");
+
     public boolean validateStofNummer(String stofNummer){
-        return false;
+        if(stofNummer == null){
+            return false;
+        }
+        return PATTERN.matcher(stofNummer).matches();
     }
 }
--- src/test/java/com/realdolmen/stof/StofNummerValidatorTest.java
+++ src/test/java/com/realdolmen/stof/StofNummerValidatorTest.java
@@ -11,10 +11,15 @@ public class StofNummerValidatorTest {
     public void testValidatorStofNummer(){
         StofNummerValidator validator = new StofNummerValidator();
         assertEquals(false, validator.validateStofNummer("Hello"));
-        assertEquals(false, validator.validateStofNummer("STOF/1/2/3"));
+        assertEquals(true, validator.validateStofNummer("STOF/1/2/3"));
         assertEquals(false, validator.validateStofNummer("STOF/2/3"));
         assertEquals(false, validator.validateStofNummer(null));
         assertEquals(true, validator.validateStofNummer("STOF/A/B/C"));
+        assertEquals(false, validator.validateStofNummer("STOF/AA/B/C"));
+        assertEquals(false, validator.validateStofNummer("STOF/11/2/3"));
+        assertEquals(false, validator.validateStofNummer("STOF/15/22/31"));
+        assertEquals(false, validator.validateStofNummer("STOF/15/2/31"));
+        assertEquals(false, validator.validateStofNummer("STOF/1/2/31"));
     }
 
 }
